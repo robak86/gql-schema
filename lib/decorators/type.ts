1f53dac5da2ce052c9643703243cdafb5c33f875
Tomasz Robaczewski <robak86@gmail.com>
2017-07-25 21:19:00 +0200

    Update types for typescript 2.4

--- lib/decorators/type.ts
+++ lib/decorators/type.ts
@@ -1,8 +1,8 @@
 import {ObjectTypeMetadata, TypeConfig} from "../metadata/ObjectTypeMetadata";
-import {Type as Klass} from "../utils/types";
+
 
 export const type = (config:TypeConfig<any, any> = {}):ClassDecorator => {
-    return <T_FUNCTION extends Klass<any>>(klass:T_FUNCTION):T_FUNCTION => {
+    return <TFunction extends Function>(klass:TFunction):TFunction => {
         let objectTypeMetadata = ObjectTypeMetadata.getOrCreateForClass(klass);
         objectTypeMetadata.setConfig(config);
 
